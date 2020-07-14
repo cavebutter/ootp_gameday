2fd6e3bc4c9672d439a79822927d25cffcd549a9
cavebutter <jay@realpm.net>
2020-07-14 12:43:36 -0700

    Depth charts now generated without template; entirely in xlsxwriter

--- sqlite_depth_chart.py
+++ sqlite_depth_chart.py
@@ -146,6 +146,59 @@ def write_depth(vis_headers, invis_headers, depth_chart):
         for k, col_data in enumerate(row_data):
             ws.write(i + 1, j + k, col_data, invis_table_style)
 
+###################
+# Chart Functions #
+###################
+
+# Generic Radar Chart
+def radar(tab, start_col, end_col, title,num_series):
+    i = 1
+    radar_chart = wb.add_chart({'type': 'radar'})
+    while i <= num_series:
+        radar_chart.add_series({
+            'name': '=' + tab + '!$A$' + str((i + 1)),
+            'categories': '=' + tab + '!$' + start_col + '$1:$' + end_col + '$1',
+            'values': '=' + tab + '!$' + start_col + '$' + str((i + 1)) + ':$' + end_col + '$' + str((i + 1))
+        })
+        i += 1
+        radar_chart.set_title({'name': title})
+    radar_chart.set_style(10)
+    radar_chart.set_legend({'position':'top'})
+    return radar_chart
+
+# 1 column chart
+def one_col_chart(tab,start_col, title, num_series):
+    colchart = wb.add_chart({'type': 'column'})
+    colchart.add_series({
+    'name': '=' + tab + '!$' + start_col + '$1',
+    'categories': '=' + tab + '!$A$2:$A$' + str(num_series+1),
+    'values': '=' + tab + '!$' + start_col + '$2:$' + start_col + '$' + str(num_series+1),
+    'data_labels': {'value': True}
+    })
+    colchart.set_style(2)
+    colchart.set_legend({'none':True})
+    colchart.set_title({'name': title})
+    return colchart
+
+# multi-column chart
+def mult_col_chart(tab,start_col, end_col, title, num_players):
+    colchart = wb.add_chart({'type': 'column'})
+    colchart.add_series({
+    'name': '=' + tab + '!$' + start_col + '$1',
+    'categories': '=' + tab + '!$A$2:$A$' + str(num_players+1),
+    'values': '=' + tab + '!$' + start_col + '$2:$' + start_col + '$' + str(num_players+1),
+    'data_labels': {'value': True}
+    })
+    colchart.add_series({
+        'name': '=' + tab + '!$' + end_col + '$1',
+        'categories': '=' + tab + '!$A$2:$A$' + str(num_players + 1),
+        'values': '=' + tab + '!$' + end_col + '$2:$' + end_col + '$' + str(num_players + 1),
+        'data_labels': {'value': True}
+    })
+    colchart.set_style(2)
+    colchart.set_legend({'position':'top'})
+    colchart.set_title({'name': title})
+    return colchart
 
 ##############
 # First Base #
@@ -161,12 +214,28 @@ first_base_invis_header = ['IF Range', 'IF Arm', 'DP', 'IF Error', '1B Rating', 
                            'Pow VL',
                            'Gap VL', 'Eye VL', 'K VL']
 
-#  Write data beginning at Row 2
+#  Write data
 write_depth(batter_vis_header, first_base_invis_header, first_base_depth)
+ws.set_column(0,0,16.00)
+
+# Charts
+ovr = radar('depthcahrt_1B','S','W','Batting Overall',3)
+ws.insert_chart('N2',ovr)
+vr = radar('depthcahrt_1B','X','AB','Batting vR',3)
+ws.insert_chart('N17', vr)
+vl = radar('depthcahrt_1B','AC','AG','Batting vL',3)
+ws.insert_chart('N31', vl)
+first_def = radar('depthcahrt_1B','N','R','Defense',3)
+ws.insert_chart('V2', first_def)
+woba = one_col_chart('depthcahrt_1B','J','WOBA',3)
+ws.insert_chart('V17',woba)
+wraa = one_col_chart('depthcahrt_1B','M','wRAA',3)
+ws.insert_chart('V32',wraa)
 
 ########################
 #  Get OF Depth Chart  #
 ########################
+
 ws = wb.add_worksheet('depthcahrt_of')
 cursor.execute(
     dp.outfield + str(small_db.game_year) + dp.org_language + org_param + dp.pos_language_of + dp.order_language)
@@ -176,10 +245,26 @@ of_invis_header = ['OF Range', 'OF Arm', 'OF Error', 'LF Rating', 'CF Rating', '
                    'Gap VL', 'Eye VL', 'K VL']
 #  Write Data
 write_depth(batter_vis_header, of_invis_header, of_depth)
+ws.set_column(0,0,16.00)
+
+# Charts
+ovr = radar('depthcahrt_of','T','X','Batting Overall',5)
+ws.insert_chart('N2',ovr)
+vr = radar('depthcahrt_of','Y','AC','Batting vR',5)
+ws.insert_chart('N17', vr)
+vl = radar('depthcahrt_of','AD','AH','Batting vL',5)
+ws.insert_chart('N31', vl)
+of_def = radar('depthcahrt_of','N','S','Defense',5)
+ws.insert_chart('V2', of_def)
+woba = one_col_chart('depthcahrt_of','J','WOBA',3)
+ws.insert_chart('V17',woba)
+wraa = one_col_chart('depthcahrt_of','M','wRAA',3)
+ws.insert_chart('V32',wraa)
 
 ########################
 #  Get C Depth Chart  #
 ########################
+
 ws = wb.add_worksheet('depthcahrt_c')
 cursor.execute(
     dp.catcher + str(small_db.game_year) + dp.org_language + org_param + dp.pos_language_c + dp.order_language)
@@ -189,11 +274,26 @@ c_invis_header = ['C Arm', 'C Abl', 'C Rating', 'OVR Con', 'OVR Pwr',
                   'Gap VL', 'Eye VL', 'K VL']
 #  Write Data
 write_depth(batter_vis_header, c_invis_header, c_depth)
+ws.set_column(0,0,16.00)
 
+# Charts
+ovr = radar('depthcahrt_c','Q','U','Batting Overall',3)
+ws.insert_chart('N2',ovr)
+vr = radar('depthcahrt_c','V','Z','Batting vR',3)
+ws.insert_chart('N17', vr)
+vl = radar('depthcahrt_c','AA','AE','Batting vL',3)
+ws.insert_chart('N31', vl)
+c_def = radar('depthcahrt_c','N','P','Defense',3)
+ws.insert_chart('V2', c_def)
+woba = one_col_chart('depthcahrt_c','J','WOBA',3)
+ws.insert_chart('V17',woba)
+wraa = one_col_chart('depthcahrt_c','M','wRAA',3)
+ws.insert_chart('V32',wraa)
 
 ########################
 #  Get MI Depth Chart  #
 ########################
+
 ws = wb.add_worksheet('depthcahrt_MI')
 cursor.execute(dp.mi + str(small_db.game_year) + dp.org_language + org_param + dp.pos_language_mi + dp.order_language)
 mi_depth = cursor.fetchall()
@@ -202,10 +302,25 @@ mi_invis_header = ['IF Range', 'IF Arm', 'DP', 'IF Error', '2B Rating', 'SS Rati
                    'Gap VL', 'Eye VL', 'K VL']
 #  Write Data
 write_depth(batter_vis_header, mi_invis_header, mi_depth)
+ws.set_column(0,0,16.00)
+# Charts
+ovr = radar('depthcahrt_MI','T','X','Batting Overall',3)
+ws.insert_chart('N2',ovr)
+vr = radar('depthcahrt_MI','Y','AC','Batting vR',3)
+ws.insert_chart('N17', vr)
+vl = radar('depthcahrt_MI','AD','AH','Batting vL',3)
+ws.insert_chart('N31', vl)
+mi_def = radar('depthcahrt_MI','N','S','Defense',3)
+ws.insert_chart('V2', mi_def)
+woba = one_col_chart('depthcahrt_MI','J','WOBA',3)
+ws.insert_chart('V17',woba)
+wraa = one_col_chart('depthcahrt_MI','M','wRAA',3)
+ws.insert_chart('V32',wraa)
 
 ########################
 #  Get 3B Depth Chart  #
 ########################
+
 ws = wb.add_worksheet('depthcahrt_3B')
 cursor.execute(
     dp.third_base + str(small_db.game_year) + dp.org_language + org_param + dp.pos_language_3b + dp.order_language)
@@ -215,10 +330,26 @@ third_invis_header = ['IF Range', 'IF Arm', 'DP', 'IF Error', '3B Rating', 'OVR 
                       'Gap VL', 'Eye VL', 'K VL']
 #  Write Data
 write_depth(batter_vis_header, third_invis_header, third_base_depth)
+ws.set_column(0,0,16.00)
+
+# Charts
+ovr = radar('depthcahrt_3B','S','W','Batting Overall',3)
+ws.insert_chart('N2',ovr)
+vr = radar('depthcahrt_3B','X','AB','Batting vR',3)
+ws.insert_chart('N17', vr)
+vl = radar('depthcahrt_3B','AC','AG','Batting vL',3)
+ws.insert_chart('N31', vl)
+third_def = radar('depthcahrt_3B','N','R','Defense',3)
+ws.insert_chart('V2', third_def)
+woba = one_col_chart('depthcahrt_3B','J','WOBA',3)
+ws.insert_chart('V17',woba)
+wraa = one_col_chart('depthcahrt_3B','M','wRAA',3)
+ws.insert_chart('V32',wraa)
 
 ########################
 #  Get SP Depth Chart  #
 ########################
+
 ws = wb.add_worksheet('depthcahrt_sp')
 cursor.execute(dp.sp + str(small_db.game_year) + dp.org_language + org_param + dp.pos_language_sp + dp.order_language)
 sp_depth = cursor.fetchall()
@@ -227,15 +358,47 @@ pitcher_vis_header = ['player', 'pos_name', 'abbr', 'level_id', 'g', 'IP', 'WHIP
 p_invis_header = ['STA', 'OVR Stu', 'OVR Con', 'OVR Mov', 'Stu VR', 'Con VR', 'Mov VR', 'Stu VL', 'Con VL', 'Mov VL']
 #  Write Data
 write_depth(pitcher_vis_header, p_invis_header, sp_depth)
+ws.set_column(0,0,16.00)
+
+#charts
+p_ovr = radar('depthcahrt_sp','O','R','Pitching Ratings Overall',5)
+ws.insert_chart('O2',p_ovr)
+pvr = radar('depthcahrt_sp','S','U','Pitching Ratings vR',5)
+ws.insert_chart('O17',pvr)
+pvl = radar('depthcahrt_sp','V','X','Pitching Ratings vL',5)
+ws.insert_chart('O32',pvl)
+whip = one_col_chart('depthcahrt_sp','G','WHIP',5)
+ws.insert_chart('W2',whip)
+war = one_col_chart('depthcahrt_sp','N','WAR',5)
+ws.insert_chart('W17',war)
+erafip = mult_col_chart('depthcahrt_sp','L','M','ERA- & FIP-',5)
+ws.insert_chart('W33',erafip)
 
 ########################
 #  Get BP Depth Chart  #
 ########################
-ws = wb.add_worksheet('depthcahrt_bp')
+
+ws = wb.add_worksheet('depthcahrt_rp')
 cursor.execute(dp.sp + str(small_db.game_year) + dp.org_language + org_param + dp.pos_language_bp + dp.order_language)
 bp_depth = cursor.fetchall()
 #  Write Data
 write_depth(pitcher_vis_header, p_invis_header, bp_depth)
+ws.set_column(0,0,16.00)
+
+#charts
+p_ovr = radar('depthcahrt_rp','O','R','Pitching Ratings Overall',5)
+ws.insert_chart('O2',p_ovr)
+pvr = radar('depthcahrt_rp','S','U','Pitching Ratings vR',5)
+ws.insert_chart('O17',pvr)
+pvl = radar('depthcahrt_rp','V','X','Pitching Ratings vL',5)
+ws.insert_chart('O32',pvl)
+whip = one_col_chart('depthcahrt_rp','G','WHIP',5)
+ws.insert_chart('W2',whip)
+war = one_col_chart('depthcahrt_rp','N','WAR',5)
+ws.insert_chart('W17',war)
+erafip = mult_col_chart('depthcahrt_rp','L','M','ERA- & FIP-',5)
+ws.insert_chart('W33',erafip)
+
 
 # TEST SECTION
 # print("TEST SECTION:")
